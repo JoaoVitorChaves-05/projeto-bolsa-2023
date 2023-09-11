3a13c5a8790cd289d2503584aa9e6578b5a78488
João Vitor Chaves <joaovmanciochaves@gmail.com>
2023-09-11 09:36:46 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import app from './app.js'
 import database from './src/database/database.js'
 
-(async () => await database.createTables())()
+(async () => {
+    try {
+        await database.createTables()
+    } catch (err) {
+        console.log(err)
+    }
+})()
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`> Listening on ${PORT}`))
