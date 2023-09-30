690a75709bb1d33542ff172170d9c86f9635ce22
João Vitor Chaves <joaovmanciochaves@gmail.com>
2023-09-30 15:05:57 -0300

    fix bugs to deploy

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import app from './app.js'
 import database from './src/database/database.js'
+import events from 'events'
 
 (async () => {
     try {
         await database.createTables()
+        eventEmitter.emit('connection')
     } catch (err) {
         console.log(err)
     }
 })()
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`> Listening on ${PORT}`))
+
+const eventEmitter = new events.EventEmitter()
+
+eventEmitter.on('connection', () => app.listen(PORT, () => console.log(`> Listening on ${PORT}`)))
