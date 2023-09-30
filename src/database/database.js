f6b9b844835c74d9c309f0bead53e9922905d6d6
João Vitor Chaves <joaovmanciochaves@gmail.com>
2023-09-30 12:48:12 -0300

    fix bugs to deploy

--- src/database/database.js
+++ src/database/database.js
@@ -9,6 +9,10 @@ class Database {
             dialectModule: pg,
             dialect: 'postgres',
             host: process.env.POSTGRES_HOST,
+            port: 5432,
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DATABASE
         })
         this.models = {}
         this.createTables()
